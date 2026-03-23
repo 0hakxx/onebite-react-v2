7b67baa0f405c7573237b86d6cd80bf499a7782a
yeong <yeonghak2207@gmail.com>
2026-03-23 18:50:48 +0900

    node.js 기초

---
+++ section03/chapter4/src/math.js
@@ -0,0 +1,13 @@
+// math 모듈
+export function add(a, b) {
+  return a + b;
+}
+
+export function sub(a, b) {
+  return a - b;
+}
+
+// export default 는 파일당 1개만 가능
+export default function multiply(a, b) {
+  return a * b;
+}
